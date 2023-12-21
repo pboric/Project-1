f6af97b902e914e0a3ea0e2d6f76e4e64ea077d6
pboric <112004618+pboric@users.noreply.github.com>
2023-12-21 17:20:31 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from hatchling import setup
+
+setup(
+    name="calculator",
+    version="1.0",
+    description="A simple calculator package that can perform basic arithmetic operations.",
+    author="Petar Krešimir Borić",
+    author_email="kresimirpet@gmail.com",
+    license="MIT license",
+    # Add any other arguments that you want
+)
